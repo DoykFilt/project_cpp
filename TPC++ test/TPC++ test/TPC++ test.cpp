0e29f52967eb8ac3f5a8f27bdf8bbbc755814c2f
CamilleThomeczek <camille.thomec@gmail.com>
2017-03-07 23:52:46 +0100

    Premiere partie refaite au sein d'un nouveau projet (ancien corrompu)# Please enter the commit message for your changes. Lines starting

---
+++ TPC++ test/TPC++ test/TPC++ test.cpp
@@ -0,0 +1,48 @@
+// TPC++ test.cpp : définit le point d'entrée pour l'application console.
+//
+/*
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+} */
+
+#include "Cexception.h"
+#include <iostream>
+
+using namespace std;
+
+//Valeurs d'exception
+#define ERREUR_DENOMINATEUR_NUL 1
+
+void main()
+{
+	float fDenominateur, fNumerateur;
+	while(1)
+	{
+		cout<<"valeur du numerateur"<<endl;
+		cin>>fNumerateur;
+
+		cout<<"valeur du denominateur"<<endl;
+		cin>>fDenominateur;
+
+		try
+		{
+			if(fDenominateur == 0)
+			{
+				Cexception EXCObjet;
+				EXCObjet.EXCModifier_Valeur(ERREUR_DENOMINATEUR_NUL);
+				EXCObjet.EXCModifier_Message("Division par zero impossible !");
+				throw EXCObjet;
+			}
+			cout<<"resultat division : "<<fNumerateur/fDenominateur<<endl;
+
+		}
+		catch(Cexception EXCObjet)
+		{
+			cout<<"Erreur "<<EXCObjet.EXCLire_Valeur()<< " " << EXCObjet.EXCLire_Message() << endl;
+		}
+	}
+}
